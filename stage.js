e4e923428d029ae8a73297e71822f43dae42d5ed
JuniorGoncallves <juniorgoncalves40@gmail.com>
2022-05-20 01:33:22 -0300

    Adding the stage file.

---
+++ stage.js
@@ -0,0 +1,44 @@
+let stage = [
+  {
+    title: "VEREADOR",
+    number: 5,
+    candidates: [
+      {
+        number: "12111",
+        name: "Arquiles Romênio",
+        party: "AB",
+        images: [{ url: "1.jpg", legend: "Vereador" }],
+      },
+      {
+        number: "20111",
+        name: "Antonies Wilson",
+        party: "DG",
+        images: [{ url: "2.jpg", legend: "Vereador" }],
+      },
+    ],
+  },
+  {
+    candidatos: [
+      {
+        number: "12",
+        name: "Cercílio Renô",
+        party: "AB",
+        vice: "Abigílio",
+        images: [
+          { url: "3.jpg", legend: "Prefeito" },
+          { url: "4jpg", legend: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        number: "20",
+        name: "Petrônio AkiHi",
+        party: "DG",
+        vice: "Adigael Fridigh",
+        images: [
+          { url: "5.jpg", legend: "Prefeito" },
+          { url: "6.jpg", legend: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
